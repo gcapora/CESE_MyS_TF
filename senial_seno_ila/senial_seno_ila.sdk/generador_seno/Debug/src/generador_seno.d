06e37a443710eb427a0b3cd595380693b8c19be9
gcapora <gfcaporaletti@undav.edu.ar>
2023-08-17 14:28:00 -0300

    Agregamos archivos del SDK.

---
+++ senial_seno_ila/senial_seno_ila.sdk/generador_seno/Debug/src/generador_seno.d
@@ -0,0 +1,40 @@
+src/generador_seno.o src/generador_seno.o: ../src/generador_seno.c \
+ ../../generador_seno_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../generador_seno_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../generador_seno_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../generador_seno_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../generador_seno_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../generador_seno_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../generador_seno_bsp/ps7_cortexa9_0/include/bspconfig.h \
+ ../../generador_seno_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../generador_seno_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../generador_seno_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../generador_seno_bsp/ps7_cortexa9_0/include/IP_seno.h \
+ ../../generador_seno_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../generador_seno_bsp/ps7_cortexa9_0/include/xil_assert.h
+
+../../generador_seno_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../generador_seno_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../generador_seno_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../generador_seno_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../generador_seno_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../generador_seno_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../generador_seno_bsp/ps7_cortexa9_0/include/bspconfig.h:
+
+../../generador_seno_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../generador_seno_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../generador_seno_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../generador_seno_bsp/ps7_cortexa9_0/include/IP_seno.h:
+
+../../generador_seno_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../generador_seno_bsp/ps7_cortexa9_0/include/xil_assert.h:
---
+++ senial_seno_ila/senial_seno_ila.sdk/sum/Debug/src/sum.d
@@ -0,0 +1,40 @@
+src/sum.o src/sum.o: ../src/sum.c \
+ ../../sum_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../sum_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../sum_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../sum_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../sum_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../sum_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../sum_bsp/ps7_cortexa9_0/include/bspconfig.h \
+ ../../sum_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../sum_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../sum_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../sum_bsp/ps7_cortexa9_0/include/sum_ip.h \
+ ../../sum_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../sum_bsp/ps7_cortexa9_0/include/xil_assert.h
+
+../../sum_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../sum_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../sum_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../sum_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../sum_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../sum_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../sum_bsp/ps7_cortexa9_0/include/bspconfig.h:
+
+../../sum_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../sum_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../sum_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../sum_bsp/ps7_cortexa9_0/include/sum_ip.h:
+
+../../sum_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../sum_bsp/ps7_cortexa9_0/include/xil_assert.h:
